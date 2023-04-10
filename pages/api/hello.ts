bc020e69b2f4081ef18dea1a1daa86d79b30ef99
Nicolas Vargas Ortega <nvortega92@gmail.com>
2023-04-10 23:25:52 +0200

    Add failing code

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,37 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type {NextApiRequest, NextApiResponse} from 'next'
 
 type Data = {
-  name: string
+    name: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+function getHeaders(req: NextApiRequest): Headers {
+    const myHeaders = new Headers();
+    Object.keys(req.headers).forEach(v => {
+        if (v !== 'connection') { // https://github.com/nodejs/undici/issues/1470
+            //if (v !== 'transfer-encoding') { // I don't know why this is failing since nextjs 13.3.0
+            myHeaders.append(v, req.headers[v] as string);
+            // }
+        }
+    });
+    return myHeaders;
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+    const headers = getHeaders(req);
+
+    try {
+        const resp = await fetch('https://example.com', {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(req.body)
+        });
+        console.log(resp);
+    } catch (e) {
+        console.log(e);
+    }
+
+    res.status(200).json({name: 'John Doe'})
 }
